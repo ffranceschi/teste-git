1dbc7ad1a3484e2377f993cdad85f06c1a9d52eb
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2025-04-09 15:21:43 -0300

    correcao bug

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World Turbo");
+        System.out.println("Hello World Turbo0");
         System.out.println("Hello World Turbo2");
     }
 
